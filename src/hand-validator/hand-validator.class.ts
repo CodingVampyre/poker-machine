abb0feb08e0e0d004c700bf181299a36330f87d0
CodingVampyre <tobiaskavsek@hotmail.de>
2021-03-07 15:17:03 +0100

    Update hand-validator.class.ts

--- src/hand-validator/hand-validator.class.ts
+++ src/hand-validator/hand-validator.class.ts
@@ -36,6 +36,10 @@ export class HandValidator {
         }
     }
 
+    // ***********
+    // * HELPERS *
+    // ***********
+
     /**
      * orders a hand by value and ignores colors
      * @param hand 
@@ -46,6 +50,24 @@ export class HandValidator {
             if (cardA[1] < cardB[1]) { return -1; }
             if (cardA[1] > cardB[1]) { return 1; }
             return 0;
+        });
+    }
+
+    /**
+     * returns tthe card list without the cards that should be removed
+     * @param cards 
+     * @param cardsToRemove 
+     * @returns 
+     */
+    public static removeCardsFromList(cards: Card[], ...cardsToRemove: Card[]): Card[] {
+        return cards.filter(card => {
+            for (const cardToRemove of cardsToRemove) {
+                if (
+                    card[0] === cardToRemove[0] &&
+                    card[1] === cardToRemove[1]
+                ) { return false; }
+            }
+            return true;
         });
     }
 
