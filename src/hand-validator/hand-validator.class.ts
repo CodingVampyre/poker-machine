6747bf021cbc735eba4cba361101e5c016b2dd90
CodingVampyre <tobiaskavsek@hotmail.de>
2021-03-07 22:00:55 +0100

    Update hand-validator.class.ts

--- src/hand-validator/hand-validator.class.ts
+++ src/hand-validator/hand-validator.class.ts
@@ -1,4 +1,4 @@
-import { Card, CardValue } from "../model/card.interface";
+import { Card, CardColor, CardValue } from "../model/card.interface";
 import { IHandValidationResult } from "./hand-validation-result.interface";
 
 export class HandValidator {
@@ -31,7 +31,7 @@ export class HandValidator {
         HandValidator.orderByValue(cards);
         for (let i = 0; i < cards.length - 1; i++) {
             const cardA = cards[i];
-            const cardB = cards[i+1];
+            const cardB = cards[i + 1];
             if (cardA[1] === cardB[1]) { return [cardA, cardB]; }
         }
     }
@@ -65,10 +65,33 @@ export class HandValidator {
         // check until prelast card
         for (let i = 0; i < cards.length - 2; i++) {
             // next and next next card should match with current
-            if (cards[i][1] === cards[i+1][1] && cards[i][1] === cards[i+2][1]) {
-                return [cards[i], cards[i+1], cards[i+2]];
+            if (cards[i][1] === cards[i + 1][1] && cards[i][1] === cards[i + 2][1]) {
+                return [cards[i], cards[i + 1], cards[i + 2]];
             }
         }
+        return undefined;
+    }
+
+    /**
+     * checks for straight
+     * @param cards 
+     * @returns 
+     */
+    public static hasStraight(cards: Card[]): Card[] | undefined {
+        if (cards.length < 5) { return undefined; }
+        HandValidator.orderByValue(cards, 'desc');
+        // loop in such a way there are always four cards to the left of current card
+        for (let i = 0; i < cards.length - 5; i++) {
+            // create subset of five cards
+            const subset = cards.slice(i, i + 5);
+            // check if that set is a striaght and return if so
+            const isStraight = HandValidator.isStraight(subset);
+            if (isStraight) { return subset; }
+        }
+        return undefined;
+    }
+
+    public static hasFlush(cards: Card[]): Card[] | undefined {
         return undefined;
     }
 
@@ -81,12 +104,20 @@ export class HandValidator {
      * @param hand 
      * @returns 
      */
-    public static orderByValue(cards: Card[]) {
-        cards.sort((cardA, cardB) => {
-            if (cardA[1] < cardB[1]) { return -1; }
-            if (cardA[1] > cardB[1]) { return 1; }
-            return 0;
-        });
+    public static orderByValue(cards: Card[], mode: 'asc' | 'desc' = 'asc') {
+        if (mode === 'asc') {
+            cards.sort((cardA, cardB) => {
+                if (cardA[1] < cardB[1]) { return -1; }
+                if (cardA[1] > cardB[1]) { return 1; }
+                return 0;
+            });
+        } else {
+            cards.sort((cardA, cardB) => {
+                if (cardA[1] > cardB[1]) { return -1; }
+                if (cardA[1] < cardB[1]) { return 1; }
+                return 0;
+            });
+        }
     }
 
     /**
@@ -105,6 +136,36 @@ export class HandValidator {
             }
             return true;
         });
+    }
+
+    /**
+     * checks if a set of five cards is a straight
+     * @param cards 
+     * @returns 
+     */
+    private static isStraight(cards: Card[]): boolean {
+        if (cards.length !== 5) { return false; }
+        // each following card must have a value higher then the one before
+        let lastValue = cards[0][1];
+        for (let i = 1; i < cards.length; i++) {
+            if (cards[i][1] === lastValue - 1) { lastValue = cards[i][1]; }
+            else { return false; }
+        }
+        return true;
+    }
+
+    /**
+     * counts a specific color in a set of cards
+     * @param cards 
+     * @param colorToCount 
+     * @returns 
+     */
+    private static countColors(cards: Card[], colorToCount: CardColor): number {
+        let count = 0;
+        for (const card of cards) {
+            if (card[0] === colorToCount) { count++; }
+        }
+        return count;
     }
 
 }
