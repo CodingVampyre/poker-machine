2e1ee56e19c8349550e1cc508eb029c80ff15569
CodingVampyre <tobiaskavsek@hotmail.de>
2021-03-11 00:20:57 +0100

    Update hand-validation.class.2.spec.ts

--- src/hand-validator/hand-validation.class.2.spec.ts
+++ src/hand-validator/hand-validation.class.2.spec.ts
@@ -3,147 +3,149 @@ import {HandValidator} from "./hand-validator.class";
 import {Hand} from "./hand-validation-result.interface";
 
 it('should evaluate high cards', () => {
-    const hand: [Card, Card] = [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.HEARTS, CardValue.TWO]];
-    const board: Card[] = [
-        [CardColor.HEARTS, CardValue.FOUR],
-        [CardColor.SPADES, CardValue.SIX],
-        [CardColor.DIAMONDS, CardValue.QUEEN],
-        [CardColor.DIAMONDS, CardValue.ACE],
-        [CardColor.SPADES, CardValue.THREE],
-    ];
-    const result = HandValidator.validateHand(hand, board);
-    expect(result).toStrictEqual({
-        hand: Hand.HIGH_CARD,
-        highCard: [CardColor.HEARTS, CardValue.SEVEN],
-        result: [[CardColor.HEARTS, CardValue.SEVEN]],
-    });
+	const hand: [Card, Card] = [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.HEARTS, CardValue.TWO]];
+	const board: Card[] = [
+		[CardColor.HEARTS, CardValue.FOUR],
+		[CardColor.SPADES, CardValue.SIX],
+		[CardColor.DIAMONDS, CardValue.QUEEN],
+		[CardColor.DIAMONDS, CardValue.ACE],
+		[CardColor.SPADES, CardValue.THREE],
+	];
+	const result = HandValidator.validateHand(hand, board);
+	expect(result).toStrictEqual({
+		hand: Hand.HIGH_CARD,
+		highCard: [CardColor.HEARTS, CardValue.SEVEN],
+		result: [[CardColor.HEARTS, CardValue.SEVEN]],
+	});
 });
 
 it('should evaluate pairs', () => {
-    const hand: [Card, Card] = [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.HEARTS, CardValue.TWO]];
-    const board: Card[] = [
-        [CardColor.HEARTS, CardValue.FOUR],
-        [CardColor.SPADES, CardValue.SIX],
-        [CardColor.DIAMONDS, CardValue.QUEEN],
-        [CardColor.DIAMONDS, CardValue.SEVEN],
-        [CardColor.SPADES, CardValue.THREE],
-    ];
-    const result = HandValidator.validateHand(hand, board);
-    expect(result).toStrictEqual({
-        hand: Hand.ONE_PAIR,
-        highCard: [CardColor.HEARTS, CardValue.SEVEN],
-        result: [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.DIAMONDS, CardValue.SEVEN],],
-    });
+	const hand: [Card, Card] = [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.HEARTS, CardValue.TWO]];
+	const board: Card[] = [
+		[CardColor.HEARTS, CardValue.FOUR],
+		[CardColor.SPADES, CardValue.SIX],
+		[CardColor.DIAMONDS, CardValue.QUEEN],
+		[CardColor.DIAMONDS, CardValue.SEVEN],
+		[CardColor.SPADES, CardValue.THREE],
+	];
+	const result = HandValidator.validateHand(hand, board);
+	expect(result).toStrictEqual({
+		hand: Hand.ONE_PAIR,
+		highCard: [CardColor.HEARTS, CardValue.SEVEN],
+		result: [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.DIAMONDS, CardValue.SEVEN],],
+	});
 });
 
 it('should evaluate two pairs', () => {
-    const hand: [Card, Card] = [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.HEARTS, CardValue.TWO]];
-    const board: Card[] = [
-        [CardColor.HEARTS, CardValue.FOUR],
-        [CardColor.SPADES, CardValue.SIX],
-        [CardColor.DIAMONDS, CardValue.TWO],
-        [CardColor.DIAMONDS, CardValue.SEVEN],
-        [CardColor.SPADES, CardValue.THREE],
-    ];
-    const result = HandValidator.validateHand(hand, board);
-    expect(result).toStrictEqual({
-        hand: Hand.TWO_PAIRS,
-        highCard: [CardColor.HEARTS, CardValue.SEVEN],
-        result: [
-            [CardColor.HEARTS, CardValue.SEVEN],
-            [CardColor.DIAMONDS, CardValue.SEVEN],
-            [CardColor.HEARTS, CardValue.TWO],
-            [CardColor.DIAMONDS, CardValue.TWO],
-        ],
-    });
+	const hand: [Card, Card] = [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.HEARTS, CardValue.TWO]];
+	const board: Card[] = [
+		[CardColor.HEARTS, CardValue.FOUR],
+		[CardColor.SPADES, CardValue.SIX],
+		[CardColor.DIAMONDS, CardValue.TWO],
+		[CardColor.DIAMONDS, CardValue.SEVEN],
+		[CardColor.SPADES, CardValue.THREE],
+	];
+	const result = HandValidator.validateHand(hand, board);
+	expect(result).toStrictEqual({
+		hand: Hand.TWO_PAIRS,
+		highCard: [CardColor.HEARTS, CardValue.SEVEN],
+		result: [
+			[CardColor.HEARTS, CardValue.SEVEN],
+			[CardColor.DIAMONDS, CardValue.SEVEN],
+			[CardColor.HEARTS, CardValue.TWO],
+			[CardColor.DIAMONDS, CardValue.TWO],
+		],
+	});
 });
 
 it('should evaluate triplets', () => {
-    const hand: [Card, Card] = [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.HEARTS, CardValue.TWO]];
-    const board: Card[] = [
-        [CardColor.HEARTS, CardValue.FOUR],
-        [CardColor.SPADES, CardValue.SIX],
-        [CardColor.SPADES, CardValue.SEVEN],
-        [CardColor.DIAMONDS, CardValue.EIGHT],
-        [CardColor.DIAMONDS, CardValue.SEVEN],
-    ];
-    const result = HandValidator.validateHand(hand, board);
-    expect(result).toStrictEqual({
-        hand: Hand.THREE_OF_A_KIND,
-        highCard: [CardColor.HEARTS, CardValue.SEVEN],
-        result: [
-            [CardColor.HEARTS, CardValue.SEVEN],
-            [CardColor.SPADES, CardValue.SEVEN],
-            [CardColor.DIAMONDS, CardValue.SEVEN],
-        ],
-    });
+	const hand: [Card, Card] = [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.HEARTS, CardValue.TWO]];
+	const board: Card[] = [
+		[CardColor.HEARTS, CardValue.FOUR],
+		[CardColor.SPADES, CardValue.SIX],
+		[CardColor.SPADES, CardValue.SEVEN],
+		[CardColor.DIAMONDS, CardValue.EIGHT],
+		[CardColor.DIAMONDS, CardValue.SEVEN],
+	];
+	const result = HandValidator.validateHand(hand, board);
+	expect(result).toStrictEqual({
+		hand: Hand.THREE_OF_A_KIND,
+		highCard: [CardColor.HEARTS, CardValue.SEVEN],
+		result: [
+			[CardColor.HEARTS, CardValue.SEVEN],
+			[CardColor.SPADES, CardValue.SEVEN],
+			[CardColor.DIAMONDS, CardValue.SEVEN],
+		],
+	});
 });
 
 it('should have a straight', () => {
-    const hand: [Card, Card] = [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.HEARTS, CardValue.FIVE]];
-    const board: Card[] = [
-        [CardColor.SPADES, CardValue.SIX],
-        [CardColor.HEARTS, CardValue.FOUR],
-        [CardColor.SPADES, CardValue.NINE],
-        [CardColor.DIAMONDS, CardValue.EIGHT],
-        [CardColor.DIAMONDS, CardValue.SEVEN],
-    ];
-    const result = HandValidator.validateHand(hand, board);
-    expect(result).toStrictEqual({
-        hand: Hand.STRAIGHT,
-        highCard: [CardColor.HEARTS, CardValue.SEVEN],
-        result: [
-            [CardColor.SPADES, CardValue.NINE],
-            [CardColor.DIAMONDS, CardValue.EIGHT],
-            [CardColor.HEARTS, CardValue.SEVEN],
-            [CardColor.SPADES, CardValue.SIX],
-            [CardColor.HEARTS, CardValue.FIVE],
-        ],
-    });
+	const hand: [Card, Card] = [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.HEARTS, CardValue.FIVE]];
+	const board: Card[] = [
+		[CardColor.SPADES, CardValue.SIX],
+		[CardColor.HEARTS, CardValue.FOUR],
+		[CardColor.SPADES, CardValue.NINE],
+		[CardColor.DIAMONDS, CardValue.EIGHT],
+		[CardColor.DIAMONDS, CardValue.SEVEN],
+	];
+	const result = HandValidator.validateHand(hand, board);
+	expect(result).toStrictEqual({
+		hand: Hand.STRAIGHT,
+		highCard: [CardColor.HEARTS, CardValue.SEVEN],
+		result: [
+			[CardColor.SPADES, CardValue.NINE],
+			[CardColor.DIAMONDS, CardValue.EIGHT],
+			[CardColor.HEARTS, CardValue.SEVEN],
+			[CardColor.SPADES, CardValue.SIX],
+			[CardColor.HEARTS, CardValue.FIVE],
+		],
+	});
 });
 
 it('should have a flush', () => {
-    const hand: [Card, Card] = [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.HEARTS, CardValue.FIVE]];
-    const board: Card[] = [
-        [CardColor.HEARTS, CardValue.SIX],
-        [CardColor.HEARTS, CardValue.QUEEN],
-        [CardColor.HEARTS, CardValue.NINE],
-        [CardColor.DIAMONDS, CardValue.EIGHT],
-        [CardColor.DIAMONDS, CardValue.SEVEN],
-    ];
-    const result = HandValidator.validateHand(hand, board);
-    expect(result).toStrictEqual({
-        hand: Hand.FLUSH,
-        highCard: [CardColor.HEARTS, CardValue.SEVEN],
-        result: [
-            [CardColor.HEARTS, CardValue.QUEEN],
-            [CardColor.HEARTS, CardValue.NINE],
-            [CardColor.HEARTS, CardValue.SEVEN],
-            [CardColor.HEARTS, CardValue.SIX],
-            [CardColor.HEARTS, CardValue.FIVE],
-        ],
-    });
+	const hand: [Card, Card] = [[CardColor.HEARTS, CardValue.SEVEN], [CardColor.HEARTS, CardValue.FIVE]];
+	const board: Card[] = [
+		[CardColor.HEARTS, CardValue.SIX],
+		[CardColor.HEARTS, CardValue.QUEEN],
+		[CardColor.HEARTS, CardValue.NINE],
+		[CardColor.DIAMONDS, CardValue.EIGHT],
+		[CardColor.DIAMONDS, CardValue.SEVEN],
+	];
+	const result = HandValidator.validateHand(hand, board);
+	expect(result).toStrictEqual({
+		hand: Hand.FLUSH,
+		highCard: [CardColor.HEARTS, CardValue.SEVEN],
+		result: [
+			[CardColor.HEARTS, CardValue.QUEEN],
+			[CardColor.HEARTS, CardValue.NINE],
+			[CardColor.HEARTS, CardValue.SEVEN],
+			[CardColor.HEARTS, CardValue.SIX],
+			[CardColor.HEARTS, CardValue.FIVE],
+		],
+	});
 });
 
-it('should have a full house', () => {
-    const hand: [Card, Card] = [[CardColor.DIAMONDS, CardValue.QUEEN], [CardColor.HEARTS, CardValue.FIVE]];
-    const board: Card[] = [
-        [CardColor.DIAMONDS, CardValue.FIVE],
-        [CardColor.HEARTS, CardValue.QUEEN],
-        [CardColor.CLUBS, CardValue.QUEEN],
-        [CardColor.DIAMONDS, CardValue.EIGHT],
-        [CardColor.DIAMONDS, CardValue.SEVEN],
-    ];
-    const result = HandValidator.validateHand(hand, board);
-    expect(result).toStrictEqual({
-        hand: Hand.FULL_HOUSE,
-        highCard: [CardColor.DIAMONDS, CardValue.QUEEN],
-        result: [
-            [CardColor.DIAMONDS, CardValue.QUEEN],
-            [CardColor.HEARTS, CardValue.QUEEN],
-            [CardColor.CLUBS, CardValue.QUEEN],
-            [CardColor.HEARTS, CardValue.FIVE],
-            [CardColor.DIAMONDS, CardValue.FIVE],
-        ],
-    });
-});
+describe('full house', () => {
+	it('should have a full house', () => {
+		const hand: [Card, Card] = [[CardColor.DIAMONDS, CardValue.QUEEN], [CardColor.HEARTS, CardValue.FIVE]];
+		const board: Card[] = [
+			[CardColor.DIAMONDS, CardValue.FIVE],
+			[CardColor.HEARTS, CardValue.QUEEN],
+			[CardColor.CLUBS, CardValue.QUEEN],
+			[CardColor.DIAMONDS, CardValue.EIGHT],
+			[CardColor.DIAMONDS, CardValue.SEVEN],
+		];
+		const result = HandValidator.validateHand(hand, board);
+		expect(result).toStrictEqual({
+			hand: Hand.FULL_HOUSE,
+			highCard: [CardColor.DIAMONDS, CardValue.QUEEN],
+			result: [
+				[CardColor.DIAMONDS, CardValue.QUEEN],
+				[CardColor.HEARTS, CardValue.QUEEN],
+				[CardColor.CLUBS, CardValue.QUEEN],
+				[CardColor.HEARTS, CardValue.FIVE],
+				[CardColor.DIAMONDS, CardValue.FIVE],
+			],
+		});
+	});
+})
