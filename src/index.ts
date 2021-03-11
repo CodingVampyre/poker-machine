67205bea991f834d9b29f0dd1a7597ea3d757d5b
Tobias Kavsek <tobiaskavsek@hotmail.de>
2021-03-11 15:09:29 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 /** Hand Validation */
 export * from './hand-validator/hand-validator.class';
 export * from './hand-validator/hand-validation-result.interface';
+
+/** deck and shuffle */
+export * from './card-deck/create-deck.function';
+export * from './card-deck/deck-shuffle.function';
