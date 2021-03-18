a240674d11d7f93efe7d3011c11e3c575fe66a53
CodingVampyre <tobiaskavsek@hotmail.de>
2021-03-18 15:49:32 +0100

    Update blackbox.class.spec.ts

--- src/blackbox/spec/game-1/blackbox.class.spec.ts
+++ src/blackbox/spec/game-1/blackbox.class.spec.ts
@@ -1,5 +1,5 @@
 import { Bundles } from "./table1.data";
-import {BlackBox} from "../../blackbox.class";
+import { BlackBox } from "../../blackbox.class";
 
 it.each(Bundles)('should simulate a complete game (Step %#)', (current) => {
     expect(BlackBox.calculateNextState(current.action, current.tableBefore)).toStrictEqual(current.tableAfter);
