6ba7982226d1e0205920858d639df219b85d0d54
CodingVampyre <tobiaskavsek@hotmail.de>
2021-03-19 23:03:38 +0100

    Update pot.interface.ts

--- src/model/pot.interface.ts
+++ src/model/pot.interface.ts
@@ -4,4 +4,7 @@ export interface IPot {
     forPlayers?: number[];
     // sum of all bets the players made
     amount: number;
+    // when a player goes all in, the current pot will have his all in value as cap
+    // everything above that gets added to a new side pot!
+    potCap?: number;
 }
