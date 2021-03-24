bd30a43757efdb0e2643aae82f154b0470be81dc
CodingVampyre <tobiaskavsek@hotmail.de>
2021-03-24 18:23:46 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /** Hand Validation */
 export * from './hand-validator/hand-validator.class';
 export * from './hand-validator/hand-validation-result.interface';
+export * from './winner-determinator/determine-winner.function';
 
 /** deck and shuffle */
 export * from './card-deck/create-deck.function';
