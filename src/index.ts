e96cfe6c7b6126e8ffc88e7d3f80fb68539a32fb
CodingVampyre <tobiaskavsek@hotmail.de>
2021-03-29 01:33:11 +0200

    :wrench: export model

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,11 @@ export * from './card-deck/deck-shuffle.function';
 
 /** Blackbox */
 export * from './blackbox/blackbox.class';
+
+/** Model */
+export * from './model/table.interface';
+export * from './model/board.interface';
+export * from './model/player.interface';
+export * from './model/player-action.interface';
+export * from './model/pot.interface';
+export * from './model/table.interface';
